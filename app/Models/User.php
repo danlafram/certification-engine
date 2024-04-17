02c9010c8224ef2080b5246cd44ed4e7e0ef082c
Daniel Laframboise <daniel.laframboise@rewind.com>
2024-04-16 20:50:40 -0400

    Remove email verified check

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@spoke.dev') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@spoke.dev');
     }
 }
