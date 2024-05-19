2f311165370906016c31015b738bfc302c5ced70
Daniel Laframboise <daniel.laframboise@rewind.com>
2024-05-18 22:12:37 -0400

    Add API route for apple connect events

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\QuizController;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/apple', function (Request $request) {
+    Log::debug('Received install info from apple');
+    Log::debug("Body: " . $request->getContent());
+    return response()->json(['success' => 'success'], 200);
+});
 
 Route::controller(AppController::class)->group(function () {
     Route::get('/app', 'index');
